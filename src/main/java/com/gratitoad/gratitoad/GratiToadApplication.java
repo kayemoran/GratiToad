5e9260355952706b61dc308d9071a3d9ed782189
Kaye Rubiene Cariaga Moran <kayemoran01@gmail.com>
2025-05-26 12:55:50 +0200

    Initial commit

---
+++ src/main/java/com/gratitoad/gratitoad/GratiToadApplication.java
@@ -0,0 +1,13 @@
+package com.gratitoad.gratitoad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GratiToadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GratiToadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gratitoad/gratitoad/GratiToadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gratitoad.gratitoad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GratiToadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
