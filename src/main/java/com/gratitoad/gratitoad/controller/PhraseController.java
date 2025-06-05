3180c119e95c271283643a8359b50febaca7594f
Nabutte <stuff___3@hotmail.com>
2025-06-05 10:11:17 +0200

    Fixed path to be /api/phrases and /api/users and some cleanup

--- src/main/java/com/gratitoad/gratitoad/controller/PhraseController.java
+++ src/main/java/com/gratitoad/gratitoad/controller/PhraseController.java
@@ -12,12 +12,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:8080")
 public class PhraseController {
     @Autowired
     private PhraseService phraseService;
 
-    
+
     @GetMapping("/phrases")
     public ResponseEntity<?> getAllPhrases() {
         try {
@@ -56,7 +57,7 @@ public class PhraseController {
             return ResponseEntity.status(404).body("Phrase with id " + id + " not found");
         }
     }
-      @PutMapping("/phrases/{id}")
+    @PutMapping("/phrases/{id}")
     public ResponseEntity<?> updatePhrase(@PathVariable Integer id, @RequestBody Phrase phrase) { //@Valid??? Med @Valid får Spring automatiskt valideringsfel, läggs till sen.
         try {
             Phrase updated = phraseService.updatePhrase(id, phrase);
@@ -123,7 +124,8 @@ public class PhraseController {
             return ResponseEntity.status(500).body("Error retrieving negative phrases: " + e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.status(500).body("Internal server error while retrieving negative phrases");
-
+        }
+    }
     @GetMapping("/phrases/{id}")
     public ResponseEntity<Phrase> getPhraseById(@PathVariable Integer id) {
         try {
@@ -134,6 +136,5 @@ public class PhraseController {
 
         }
     }
-
 
 }
